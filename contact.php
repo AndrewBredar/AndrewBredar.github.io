fb8ba50dee5832859fdf5e9cbc69dbe5d09074aa
Andrew Bredar <abredar3@gmail.com>
2020-08-13 16:32:31 -0700

    Contact form

--- contact.php
+++ contact.php
@@ -1,16 +1,13 @@
 <?php
-/*
- *  CONFIGURE EVERYTHING HERE
- */
 
 // an email address that will be in the From field of the email.
 $from = 'Demo contact form <demo@domain.com>';
 
 // an email address that will receive the email with the output of the form
-$sendTo = 'Demo contact form <bikas9971@gmail.com>';
+$sendTo = 'abredar@scu.edu';
 
 // subject of the email
-$subject = 'New message from contact form';
+$subject = 'Contact Form Message!';
 
 // form field names and their translations.
 // array variable name => Text to appear in the email
@@ -23,7 +20,7 @@ $okMessage = 'Contact form successfully submitted. Thank you, I will get back to
 $errorMessage = 'There was an error while submitting the form. Please try again later';
 
 /*
- *  LET'S DO THE SENDING
+ *  Sending part
  */
 
 // if you are not debugging and don't need error reporting, turn this off by error_reporting(0);
